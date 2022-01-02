40207d9524c6cbde14524e00786c369dea843c26
Rifka Annisa <Geminigirl124@gmail.com>
2022-01-02 12:16:34 +0700

    ubah karyawan

--- controllers/karyawan.js
+++ controllers/karyawan.js
@@ -25,7 +25,7 @@ exports.getKaryawan = (req,res) => {
     tanggalbergabung:tanggalbergabung
     })
     .then(result => {
-        res.send("Data Karyawan sudah ditambahkan ")
+        res.send("Data Karyawan sudah ditambahkan ");
     //   res.redirect('/admin/checklistmeja');
     }
     ).catch(err => console.log(err));
@@ -41,10 +41,35 @@ exports.getKaryawan = (req,res) => {
         return hasil.destroy();
       })
       .then(result => {
-        res.send("Data Karyawan sudah dihapus ")
+        res.send("Data Karyawan sudah dihapus ");
 
         // res.redirect('/admin/buktiTemuanruang');
       })
       .catch(err => console.log(err));
+  };
+  
+  exports.putEditKaryawan = (req,res,next) => {
+    const id = req.params.id;
+    const nama = req.body.nama;
+    const alamat = req.body.alamat;
+    const tanggallahir = req.body.tanggallahir;
+    const tanggalbergabung = req.body.tanggalbergabung;  
+    
+    Karyawan.findByPk(id)
+    .then(karyawan => {
+        karyawan.nama = nama;
+        karyawan.alamat = alamat;
+        karyawan.tanggallahir = tanggallahir;
+        karyawan.tanggalbergabung = tanggalbergabung;
+
+      return karyawan.save();
+    })
+    .then(result => {
+    //   res.redirect('/admin/checklistmeja');
+    res.send("Data Karyawan sudah diubah ")
+    
+    })
+    .catch(err => console.log(err));
+  
   };
   
--- routes/karyawan.js
+++ routes/karyawan.js
@@ -7,6 +7,8 @@ const router = express.Router();
 router.get('/', karyawanController.getKaryawan);
 router.post('/add', karyawanController.postAddKaryawan);
 router.delete('/delete/:id', karyawanController.deleteKaryawan);
+router.put('/edit/:id', karyawanController.putEditKaryawan);
+
 
 
 module.exports = router;
